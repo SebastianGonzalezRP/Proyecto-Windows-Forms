1bd52855356232cbeec81c3efa2e74d3cbc7aed1
exequielvial <ejvial@miuandes.cl>
2018-05-08 22:04:31 -0300

    salud2

---
+++ Menus/MenuAdmin/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sayōnara");
+            this.Close();
+        }
+    }
+}
